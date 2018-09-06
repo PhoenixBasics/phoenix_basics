38fc001cd44e481e21fbb699a56ccdf4d2dfb96c
Brian E. McElaney <brian.e.mcelaney@gmail.com>
2018-09-06 14:13:24 -0700

    Add Basics.Auth.authenticate_user()

--- lib/phoenix_basics/auth/auth.ex
+++ lib/phoenix_basics/auth/auth.ex
@@ -4,9 +4,9 @@ defmodule Basics.Auth do
   """
 
   import Ecto.Query, warn: false
-  alias Basics.Repo
-
   alias Basics.Auth.User
+  alias Basics.Repo
+  alias Comeonin.Bcrypt
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking user changes.
@@ -20,4 +20,35 @@ defmodule Basics.Auth do
   def change_user(%User{} = user) do
     User.changeset(user, %{})
   end
+
+  def authenticate_user(%{"username" => user, "password" => pass}) do
+    authenticate_user(%{username: user, password: pass})
+  end
+
+  def authenticate_user(%{username: user, password: pass}) do
+    user
+    |> fetch_user_by_username()
+    |> check_password(pass)
+  end
+
+  defp fetch_user_by_username(username) do
+    User
+    |> where([user], user.username == ^username)
+    |> Repo.one()
+  end
+
+  defp check_password(%User{} = user, password) do
+    password
+    |> Bcrypt.checkpw(user.password)
+    |> case do
+      true ->
+        {:ok, user}
+
+      false ->
+        Bcrypt.dummy_checkpw()
+        {:error, change_user(%User{})}
+    end
+  end
+
+  defp check_password(_, _), do: {:error, change_user(%User{})}
 end
--- test/phoenix_basics/auth/auth_test.exs
+++ test/phoenix_basics/auth/auth_test.exs
@@ -2,12 +2,43 @@ defmodule Basics.AuthTest do
   use Basics.DataCase
 
   alias Basics.Auth
+  alias Basics.Signup
+  alias Ecto.Changeset
 
-  describe "users" do
+  @valid_attrs %{password: "some password", username: "some username"}
+
+  describe "Given a user exists in the database" do
     alias Basics.Auth.User
 
+    setup do
+      {:ok, user} = Signup.create_user(@valid_attrs)
+
+      [user: user]
+    end
+
+    test "authenticate_user/1 returns an :ok tuple on success", %{user: user} do
+      {:ok, result} = Auth.authenticate_user(@valid_attrs)
+
+      assert %User{} = result
+      assert result.id == user.id
+    end
+
+    test "authenticate_user/1 returns an :error tuple if user not found" do
+      invalid = Map.put(@valid_attrs, :username, "Some other user")
+      {:error, %Changeset{} = change} = Auth.authenticate_user(invalid)
+
+      assert is_nil(change.action)
+    end
+
+    test "authenticate_user/1 returns an :error tuple if password incorrect" do
+      invalid = Map.put(@valid_attrs, :password, "Some other password")
+      {:error, %Changeset{} = change} = Auth.authenticate_user(invalid)
+
+      assert is_nil(change.action)
+    end
+
     test "change_user/1 returns a user changeset" do
-      assert %Ecto.Changeset{} = Auth.change_user(%User{})
+      assert %Changeset{} = Auth.change_user(%User{})
     end
   end
 end
